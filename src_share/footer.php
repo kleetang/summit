7985f095f8160afbec8ac3116f7e5f53de5a46b3
Klee <kleetang@gmail.com>
2014-07-28 17:30:34 -0400

    first version

---
+++ src_share/footer.php
@@ -0,0 +1,48 @@
+    <footer>
+      <div class="footer-top">
+        <div class="footer-inner">
+        </div>
+        <div class="footer-content">
+          <div class="container">
+              <div class="row">
+                
+                <div class="col-md-3 left-icon">
+                  <div class="footer-left">
+                    <div class="col-sx-12"><h3>Contact Us:</h3></div>
+                    <div class="col-sx-12">American Education Center</div>
+                    <div class="col-sx-12">Phone: 1-212-825-0437</div>
+                    <br>
+                    <div class="row">
+                      <div class="col-md-3 icon_one"><a class="twitter icon_show" title="twitter" href="https://twitter.com/AEILearnChinese">twitter</a></div>
+                      <div class="col-md-3 icon_two"><a class="twitter icon_show" title="twitter" href="https://twitter.com/AEILearnChinese">twitter</a></div>
+                      <div class="col-md-3 icon_three"><a class="twitter icon_show" title="twitter" href="https://twitter.com/AEILearnChinese">twitter</a></div>
+                  </div>
+                  </div>
+                </div>
+                
+                <div class="col-md-3 contact-info">
+                  <h2 itemprop="name">Location:</h2>
+                  <p>
+                    <span itemprop="streetAddress">17 Battery PL</span><br>
+                    <span itemprop="addressLocality">New York</span>, <span itemprop="addressRegion">NY</span> <span itemprop="postalCode">10001</span> <br>
+                  </p>
+                </div>                
+                <div class="col-md-6 map">
+                  <div id="location-map">
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="copyright">
+            Copyright © 2014 American Education Center
+          </div>
+          <ul class="footer-nav">
+            <li><a href="summit.html">Back to top</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
